3fcd4502ea5d25fcd9a90532d455037acc061217
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2020-10-12 15:43:56 -0500

    full MOVIES data is returned for endpoint

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const MOVIES = require('./movies-data-small.json');
 
 const app = express();
 
@@ -10,8 +11,6 @@ app.use(function validateBearerToken(req, res, next) {
   const authToken = req.get('Authorization');
   const apiToken = process.env.API_TOKEN;
 
-  console.log('validateBearerToken');
-
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' });
   }
@@ -19,7 +18,7 @@ app.use(function validateBearerToken(req, res, next) {
 })
 
 function handleGetMovie(req, res) {
-  res.send('this endpoint is working');
+  res.json(MOVIES);
 };
 
 app.get('/movie', handleGetMovie)
